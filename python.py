ab63d22c52b298db130621ba3b235c5babc05e3c
Salman <msalmanbappy@gmail.com>
2024-12-06 17:04:56 +0600

    initial

---
+++ python.py
@@ -0,0 +1,10 @@
+# (a*b) + (c*d)
+a = int(input('Enter the value of a : '))
+b = int(input('Enter the value of b : '))
+c = int(input('Enter the value of c : '))
+d = int(input('Enter the value of d : '))
+s1 = a * b
+s2 = c * d
+# we did addition in this line
+result = s1 + s2
+print("The result is = ", result)
